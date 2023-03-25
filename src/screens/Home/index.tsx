52c4abd13b4de49b9b04076d5490cdf730024804
Emerson Silva <emerson_jdss@hotmail.com>
2023-03-25 16:50:59 -0300

    adjusts on Home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,50 @@
 import React from 'react';
-import { View } from 'react-native';
+import { SafeAreaView, View } from 'react-native';
+import { Box } from '../../components/Spacing';
+import { Avatar, Divider } from 'react-native-paper';
+import { Title } from '../../components/Texts';
+import { Ionicons } from '@expo/vector-icons';
 
-// import { Container } from './styles';
 
 const Home: React.FC = () => {
-    return <View />;
+    return (
+        <>
+            <SafeAreaView>
+                <Box height="100%" width="100%" backgroundColor="white">
+                    <Box mt={10}
+                        ml={10}
+                        flexDirection="row"
+                        alignItems="center"
+                    >
+                        <Box>
+                            <Avatar.Image size={50} source={require('../../assets/terapeuta.png')} />
+                        </Box>
+                        <Box
+                            ml={10}
+                        >
+                            <Title>
+                                Olá, Marcela!
+                            </Title>
+                        </Box>
+                    </Box>
+                    <Box
+                        mt={10}
+                        ml={10}
+                    >
+                        <Divider />
+                    </Box>
+                    <Box
+                        mt={10}
+                        ml={10}
+                    >
+                        <Title>
+                            Atendimentos do Dia
+                        </Title>
+                    </Box>
+                </Box>
+            </SafeAreaView>
+        </>
+    )
 }
 
 export default Home;
