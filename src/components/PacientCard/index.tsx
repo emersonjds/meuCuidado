efe3cec8eb731e8bb8c7f5aef0ee9cb73a126cc6
Emerson Silva <emerson_jdss@hotmail.com>
2023-03-25 17:29:43 -0300

    create patientCard

--- src/components/PacientCard/index.tsx
+++ src/components/PacientCard/index.tsx
@@ -1,10 +1,55 @@
 import React from "react";
-import { View } from "react-native";
+import {Box} from "../Spacing";
+import {Subtitle, Title} from "../Texts";
+import {Avatar} from "react-native-paper";
 
-// import { Container } from './styles';
 
-const PacientCard: React.FC = () => {
-  return <View />;
+type PacientCardProps = {
+    patients: {
+        name: string;
+        time: string;
+    }[]
+}
+
+const PacientCard: React.FC<PacientCardProps> = (
+    {
+        patients
+    }
+) => {
+    return (
+        <>
+            <Box
+                mr={10}
+                p={10}
+                borderWidth={1}
+                borderRadius={10}
+            >
+                <Title>
+                    Marcela Pereira
+                </Title>
+                <Title>
+                    Horario: 10:00
+                </Title>
+                <Box
+                    flexDirection="row"
+                    alignItems="center"
+                    mt={10}
+                >
+                    <Avatar.Icon size={50} icon="whatsapp"/>
+                    <Box
+                        ml={10}
+                    >
+                        <Subtitle>
+                            Entrar em Contato
+                        </Subtitle>
+                    </Box>
+
+                </Box>
+
+            </Box>
+
+        </>
+    );
 };
 
 export default PacientCard;
