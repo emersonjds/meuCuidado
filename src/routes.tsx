21f8faaec40aba6fc6da329deb9830d4cc42937a
Emerson Silva <emerson_jdss@hotmail.com>
2023-03-25 17:36:29 -0300

    config data on Routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-import {NavigationContainer} from "@react-navigation/native";
-import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
-import {AntDesign} from "@expo/vector-icons";
-import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { AntDesign } from "@expo/vector-icons";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Home from "./screens/Home";
 import HomeScreen from "./screens/Home";
 
@@ -11,24 +11,32 @@ const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
 const MyTabs = () => {
-    return (<Tab.Navigator>
-            <Tab.Screen name="Home" component={HomeScreen}
-                        options={{
-                            tabBarLabel: "Home",
-                            tabBarLabelStyle: {
-                                color: "#000",
-                            },
-                            headerShown: false,
-                            tabBarIcon: ({color}) => (<AntDesign name="home" size={24} color="black"/>),
-                        }}
-            />
-        </Tab.Navigator>)
-}
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarLabel: "Home",
+          tabBarLabelStyle: {
+            color: "#000",
+          },
+          headerShown: false,
+          tabBarIcon: ({ color }) => (
+            <AntDesign name="home" size={24} color="black" />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
 
 const Routes = () => {
-    return (<NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name="Home" component={MyTabs}/>
-            </Stack.Navigator>
-        </NavigationContainer>)
-}
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={MyTabs} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
