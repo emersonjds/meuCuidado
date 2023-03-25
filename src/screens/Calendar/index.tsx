9da0dada0a48b3c2d51cda9f88ee31aff2b34e48
Emerson Silva <emerson_jdss@hotmail.com>
2023-03-25 18:38:27 -0300

    adjust Calendar Screen

--- src/screens/Calendar/index.tsx
+++ src/screens/Calendar/index.tsx
@@ -1,10 +1,85 @@
-import React from "react";
-import { View } from "react-native";
+import React, { useState } from "react";
+import { Text, View } from "react-native";
+import { Calendar, LocaleConfig } from "react-native-calendars";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Title } from "../../components/Texts";
+import { Box } from "../../components/Spacing";
+import { Divider } from "react-native-paper";
 
-// import { Container } from './styles';
+const CalendarScreen: React.FC = () => {
+  const [selected, setSelected] = useState("");
 
-const Calendar: React.FC = () => {
-  return <View />;
+  LocaleConfig.locales["pt-br"] = {
+    monthNames: [
+      "Janeiro",
+      "Fevereiro",
+      "Março",
+      "Abril",
+      "Maio",
+      "Junho",
+      "Julho",
+      "Agosto",
+      "Setembro",
+      "Outubro",
+      "Novembro",
+      "Dezembro",
+    ],
+  };
+
+  const onDayPress = (day) => {
+    setSelected(day.dateString);
+  };
+
+  //   create a function that receive the data and push item in array of dates
+  const handleDate = (date) => {
+    const newDate = date;
+    const newDates = [...dates, newDate];
+    setDates(newDates);
+  };
+
+  return (
+    <>
+      <SafeAreaView
+        style={{
+          backgroundColor: "#fff",
+          paddingTop: 20,
+          paddingHorizontal: 20,
+        }}
+      >
+        <Box mt={10}>
+          <Box mb={30}>
+            <Title>Adicione datas em que você esta disponivel</Title>
+          </Box>
+
+          <Box nt={10} mb={10}>
+            <Divider />
+          </Box>
+          <Calendar
+            onDayPress={(day) => {
+              return setSelected(day.dateString);
+            }}
+            markedDates={{
+              "2023-03-27": {
+                selected: true,
+                marked: true,
+                selectedColor: "blue",
+              },
+              "2023-03-28": {
+                selected: true,
+                marked: true,
+                selectedColor: "blue",
+              },
+              "2023-03-29": {
+                selected: true,
+                marked: true,
+                selectedColor: "blue",
+              },
+            }}
+          />
+        </Box>
+      </SafeAreaView>
+    </>
+  );
 };
 
-export default Calendar;
+export default CalendarScreen;
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,55 +1,60 @@
-import React from 'react';
-import {SafeAreaView} from 'react-native';
-import {Box} from '../../components/Spacing';
-import {Avatar, Divider} from 'react-native-paper';
-import {Title} from '../../components/Texts';
+import React from "react";
+import { SafeAreaView } from "react-native";
+import { Box } from "../../components/Spacing";
+import { Avatar, Divider } from "react-native-paper";
+import { Title } from "../../components/Texts";
 import PacientCard from "../../components/PacientCard";
 
 const HomeScreen: React.FC = () => {
-    return (
-        <>
-            <SafeAreaView>
-                <Box height="100%" width="100%" backgroundColor="white">
-                    <Box mt={10}
-                         ml={10}
-                         flexDirection="row"
-                         alignItems="center"
-                    >
-                        <Box>
-                            <Avatar.Image size={50} source={require('../../assets/terapeuta.png')}/>
-                        </Box>
-                        <Box
-                            ml={10}
-                        >
-                            <Title>
-                                Olá, Marcela!
-                            </Title>
-                        </Box>
-                    </Box>
-                    <Box
-                        mt={10}
-                        ml={10}
-                    >
-                        <Divider/>
-                    </Box>
-                    <Box
-                        mt={10}
-                        ml={10}
-                    >
-                        <Title>
-                            Atendimentos do Dia
-                        </Title>
-                        <Box
-                            pt={10}
-                        >
-                            <PacientCard />
-                        </Box>
+  const patients = [
+    {
+      id: 1,
+      name: "Marcela Pereira",
+      time: "10:00",
+      phone: "+5511971801555",
+    },
+    {
+      id: 2,
+      name: "Marcela Pereira",
+      time: "10:00",
+      phone: "+5511971801555",
+    },
+    {
+      id: 3,
+      name: "Marcela Pereira",
+      time: "10:00",
+      phone: "+5511971801555",
+    },
+  ];
 
-                    </Box>
-                </Box>
-            </SafeAreaView>
-        </>
-    )
-}
+  return (
+    <>
+      <SafeAreaView>
+        <Box height="100%" width="100%" backgroundColor="white">
+          <Box mt={10} ml={10} flexDirection="row" alignItems="center">
+            <Box>
+              <Avatar.Image
+                size={50}
+                source={require("../../assets/terapeuta.png")}
+              />
+            </Box>
+            <Box ml={10}>
+              <Title>Olá, Marcela!</Title>
+            </Box>
+          </Box>
+          <Box mt={10} ml={10}>
+            <Divider />
+          </Box>
+          <Box mt={10} ml={10}>
+            <Title>Atendimentos do Dia</Title>
+            <Box pt={10}>
+              <PacientCard patients={patients} />
+            </Box>
+          </Box>
+        </Box>
+      </SafeAreaView>
+    </>
+  );
+};
 
 export default HomeScreen;
