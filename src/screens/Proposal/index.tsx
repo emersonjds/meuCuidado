aae8dce701c654b72b87a3ef9c6c2f3250b275ef
Emerson Silva <emerson_jdss@hotmail.com>
2023-03-26 05:31:18 -0300

    changes on proposal

--- src/screens/Proposal/index.tsx
+++ src/screens/Proposal/index.tsx
@@ -1,11 +1,27 @@
 import React from "react";
 import { View } from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import { Title } from "../../components/Texts";
+import { Box } from "../../components/Spacing";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Proposal: React.FC = () => {
   const navigation = useNavigation();
 
-  return <View />;
+  return (
+    <>
+      <SafeAreaView
+        style={{
+          flex: 1,
+          padding: 20,
+        }}
+      >
+        <Box flex={1}>
+          <Title>Proposal</Title>
+        </Box>
+      </SafeAreaView>
+    </>
+  );
 };
 
 export default Proposal;
